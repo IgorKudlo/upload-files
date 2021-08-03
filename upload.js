7fd04c6cd2054dac250f834e687d8076d2864767
igor.kudlo <igoreklida@gmail.com>
2021-08-03 11:01:21 +0300

    add preview uploaded images

--- upload.js
+++ upload.js
@@ -1,5 +1,8 @@
 export function upload(selector, options = {}) {
     const input = document.querySelector(selector);
+    const preview = document.createElement("div");
+
+    preview.classList.add("preview");
 
     const open = document.createElement("button");
     open.classList.add("btn");
@@ -13,12 +16,37 @@ export function upload(selector, options = {}) {
         input.setAttribute("accept", options.accept.join(","));
     }
 
+    input.insertAdjacentElement("afterend", preview);
     input.insertAdjacentElement("afterend", open);
 
     const triggerInput = () => input.click();
 
     const changeHandler = event => {
-        console.log(event)
+        if (!event.target.files.length) {
+            return;
+        }
+
+        const files = Array.from(event.target.files);
+
+        preview.innerHTML = "";
+        files.forEach(file => {
+            if (!file.type.match("image")) {
+                return;
+            }
+
+            const reader = new FileReader();
+
+            reader.onload = ev => {
+                const src = ev.target.result;
+                preview.insertAdjacentHTML("afterbegin", `
+                    <div class="preview-image">
+                        <img src="${src}" alt="${file.name}">
+                    </div>
+                `)
+            }
+
+            reader.readAsDataURL(file);
+        })
     }
 
     open.addEventListener("click", triggerInput);
